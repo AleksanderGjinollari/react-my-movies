f836b6c7f9958a7ab2e09ccc6227fe2a7294f793
aleksandergjinollari <aleksandergjinollari@hotmail.com>
2022-09-16 15:59:10 +0200

    added movie filters and fetched movies through the api

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import Movie from "./components/Movie";
+import Filter from "./components/Filter";
+import "./App.css";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [popular, setPopular] = useState([]);
+  const [filtered, setFiltered] = useState([]);
+  const [activeGenre, setActiveGenre] = useState(0);
+
+  useEffect(() => {
+    fetchPopular();
+  }, []);
+
+  const fetchPopular = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/popular?api_key=9b93f430625c03881fb5f3d7518af19d&language=en-US&page=1"
+    );
+    const movies = await data.json();
+    setPopular(movies.results);
+    setFiltered(movies.results);
+  };
+
+  return (
+    <div className="App">
+      <Filter
+        popular={popular}
+        setFiltered={setFiltered}
+        activeGenre={activeGenre}
+        setActiveGenre={setActiveGenre}
+      />
+      <div className="popular-movies">
+        {popular.map((movie) => {
+          return <Movie key={movie.id} movie={movie} />;
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
